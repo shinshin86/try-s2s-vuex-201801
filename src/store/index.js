c9a26b664b99e2cdd18e1f612a7deeb7e65da19f
shinshin86 <beagles1986@gmail.com>
2019-05-11 20:54:07 +0900

    Upgrade to babel 7

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,35 @@
-import { ADD, DONE, DELETE } from "./mutation-types";
-const state = {};
+import { ADD, DONE, DELETE } from './mutation-types'
+const state = {}
 const actions = {
   [ADD]({ commit }, state) {
-    commit(ADD, state);
+    commit(ADD, state)
   },
 
   [DONE]({ commit }, state) {
-    commit(DONE, state);
+    commit(DONE, state)
   },
 
   [DELETE]({ commit }, state) {
-    commit(DELETE, state);
+    commit(DELETE, state)
   }
-};
+}
 const mutations = {
   [ADD](state) {
-    state = state;
+    state = state
   },
 
   [DONE](state) {
-    state = state;
+    state = state
   },
 
   [DELETE](state) {
-    state = state;
+    state = state
   }
-};
-const getters = {};
+}
+const getters = {}
 export default new Vuex.Store({
   state,
   getters,
   actions,
   mutations
-});
+})
